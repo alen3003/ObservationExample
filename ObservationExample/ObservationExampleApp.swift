9e7261c63bf300cfba8e681ab3e7ce72bd4ab009
Alen Šebalj <sebalj.alen@gmail.com>
2023-10-29 23:53:27 +0100

    Initial Commit

---
+++ ObservationExample/ObservationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationExampleApp.swift
+//  ObservationExample
+//
+//  Created by Alen Sebalj on 29.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct ObservationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
